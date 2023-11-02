c05952911fc68135acbb72d24dd5c6dafbd35ad4
gabcerqueira <gabcsilva97@gmail.com>
2023-11-02 13:14:50 -0300

    auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,14 @@ import { MailingModule } from './mailing/mailing.module';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 @Module({
   imports: [
     ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.MONGODB_URL!),
     MailingModule,
     UserModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
