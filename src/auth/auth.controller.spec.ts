ecb2bff14e8d096b7154e74d29215efa29c77a0b
ropnascimento <rafael.nascimento@fluna.io>
2023-11-03 20:57:07 -0300

    Mudanças auth controller

--- src/auth/auth.controller.spec.ts
+++ src/auth/auth.controller.spec.ts
@@ -1,11 +1,12 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { JwtService } from '@nestjs/jwt';
+import { LoginDto } from './dto/login.dto';
+import { User } from '../user/schema/user.schema';
 
 describe('AuthController', () => {
-  let controller: AuthController;
-  let service: AuthService;
+  let authController: AuthController;
+  let authService: AuthService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -24,10 +25,47 @@ describe('AuthController', () => {
       ],
     }).compile();
 
-    controller = module.get<AuthController>(AuthController);
+    authController = module.get<AuthController>(AuthController);
+    authService = module.get<AuthService>(AuthService);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  describe('login', () => {
+    it('should return a token on successful login', async () => {
+      const loginDto: LoginDto = {
+        email: 'test@example.com',
+        password: 'password123',
+      };
+
+      const user: User = {
+        id: '1',
+        email: 'test@example.com',
+      };
+
+      jest.spyOn(authService, 'login').mockImplementation(async () => ({
+        access_token: 'mocked-access-token',
+      }));
+
+      const result = await authController.login({}, loginDto);
+
+      expect(result).toEqual({ access_token: 'mocked-access-token' });
+    });
+
+  });
+
+  describe('refresh', () => {
+    it('should return a new token on successful refresh', async () => {
+      const user: User = {
+        id: '1',
+        email: 'test@example.com',
+      };
+
+      jest.spyOn(authService, 'refresh').mockImplementation(async () => ({
+        access_token: 'mocked-access-token',
+      }));
+
+      const result = await authController.refresh({ user });
+
+      expect(result).toEqual({ access_token: 'mocked-access-token' });
+    });
   });
 });
